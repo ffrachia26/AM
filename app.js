8854d7c983cb96dd8affb32fcfbe2076cd0f55f3
Francisco Frachia <francisco.frachia@hotmail.com>
2022-03-25 00:33:59 -0300

    Instale Nodemon

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let express = require('express')
-let fs = require('fs')
+const express = require('express');
+let fs = require('fs');
+const app = express();
+
 
 console.log(express)
+
+app.listen(3000, function() {
+    console.log("El servidor esta corriendo");
+    console.log("http://localhost:3000")
+})
 
